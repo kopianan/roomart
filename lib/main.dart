789568805b0ee1f7d8fefdfb004533968a84bbb2
kopianan <ananalfredcarlos@gmail.com>
2021-06-22 09:30:34 +0700

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:roomart/presentation/app_widget.dart';
@@ -5,7 +7,30 @@ import 'package:roomart/presentation/app_widget.dart';
 import 'injection.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   configureDependencies();
   await GetStorage.init();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(AppWidget());
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  if (message.data['notification'] != null) {
+    final data = message.data['notification'];
+    final title = data['title'];
+    final body = data['body'];
+    await showNotificationWithDefaultSound(title, body);
+  }
+  return Future<void>.value();
+}
+
+Future showNotificationWithDefaultSound(String title, String message) async {
+  print(message);
+  
 }
--- lib/presentation/app_widget.dart
+++ lib/presentation/app_widget.dart
@@ -1,4 +1,6 @@
 import 'package:color_to_material/color_to_material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:roomart/application/auth/auth_controller.dart';
@@ -16,37 +18,79 @@ class AppWidget extends StatefulWidget {
 }
 
 class _AppWidgetState extends State<AppWidget> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   final authController = Get.put(AuthController());
 
   @override
+  void initState() {
+    
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      getPages: pages,
-      debugShowCheckedModeBanner: false,
-      initialRoute: SplashScreenPage.TAG,
-      // home: TestPage(),
-      theme: ThemeData(
-          bottomNavigationBarTheme: BottomNavigationBarThemeData(
-              backgroundColor: Colors.white, elevation: 4),
-          appBarTheme: AppBarTheme(
-              centerTitle: true,
-              elevation: 3,
-              iconTheme: IconThemeData(color: Colors.white)),
-          inputDecorationTheme: InputDecorationTheme(
-              fillColor: Colors.white,
-              filled: true,
-              labelStyle: TextStyle(color: Colors.black),
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(width: 1, color: Colors.grey)),
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(width: 1, color: Colors.grey))),
-          scaffoldBackgroundColor: Colors.white,
-          primarySwatch:
-              ColorToMaterial.fromHEX(primaryColor).getMaterialColor()),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Column(
+                  children: [Text("Firebase Error")],
+                ),
+              ),
+            ),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return GetMaterialApp(
+            getPages: pages,
+            debugShowCheckedModeBanner: false,
+            initialRoute: SplashScreenPage.TAG,
+            // home: TestPage(),
+            theme: ThemeData(
+                bottomNavigationBarTheme: BottomNavigationBarThemeData(
+                    backgroundColor: Colors.white, elevation: 4),
+                appBarTheme: AppBarTheme(
+                    centerTitle: true,
+                    elevation: 3,
+                    iconTheme: IconThemeData(color: Colors.white)),
+                inputDecorationTheme: InputDecorationTheme(
+                    fillColor: Colors.white,
+                    filled: true,
+                    labelStyle: TextStyle(color: Colors.black),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(width: 1, color: Colors.grey)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(width: 1, color: Colors.grey))),
+                scaffoldBackgroundColor: Colors.white,
+                primarySwatch:
+                    ColorToMaterial.fromHEX(primaryColor).getMaterialColor()),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Column(
+                children: [CircularProgressIndicator()],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
